9f9733299ae9e905e7f9ea88c0f7aa13346c4802
Roberto Samia <bojosteph@gmail.com>
2019-02-12 16:58:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,6 +11,12 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/checkout' do
+    @sessions = sessions
+    item = params[:item]
+    @sessions[:item] = item
+  end
     
   
 end
